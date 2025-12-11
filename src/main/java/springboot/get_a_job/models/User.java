6950522b2cf021d8cce077043df8c6e8b1ded880
name <tzolcubekov@gmail.com>
2025-12-11 20:18:05 +0600

    User class added

---
+++ src/main/java/springboot/get_a_job/models/User.java
@@ -0,0 +1,22 @@
+package springboot.get_a_job.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    public Integer id;
+    public String name;
+    public String surname;
+    public Integer age;
+    public String email;
+    public String password;
+    public String phoneNumber;
+    public String avatar;
+    public String accountType;
+}
