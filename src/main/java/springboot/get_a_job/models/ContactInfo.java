7dd5c3f0970acb27220594342c9368882639e312
name <tzolcubekov@gmail.com>
2025-12-11 20:26:27 +0600

    ContactInfo class added

---
+++ src/main/java/springboot/get_a_job/models/ContactInfo.java
@@ -0,0 +1,17 @@
+package springboot.get_a_job.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactInfo {
+    public Integer id;
+    public Integer typeId;
+    public Integer resumeId;
+    public String value;
+}
