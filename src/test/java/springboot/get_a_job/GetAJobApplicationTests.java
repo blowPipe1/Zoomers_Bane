769fa8c9ecf111658b2abd10f9313da263ee610d
name <tzolcubekov@gmail.com>
2025-12-11 19:52:42 +0600

    Init git Repo

---
+++ src/test/java/springboot/get_a_job/GetAJobApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.get_a_job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetAJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
