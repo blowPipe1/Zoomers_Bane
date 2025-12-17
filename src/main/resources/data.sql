2c81e93ce0fe9abbf00163cb8d5158851c863410
name <tzolcubekov@gmail.com>
2025-12-17 17:07:35 +0600

    data.sql file added, containing sql code for creating tables & inserting with data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,50 +1,133 @@
-create table if not exists USERS(
-                                    id identity primary key,
-                                    name varchar(45),
-                                    surName varchar(45),
-                                    age int,
-                                    email varchar(255) not null,
-                                    password varchar(255) not null,
-                                    phoneNumber varchar(15),
-                                    avatar varchar,
-                                    accountType varchar
+CREATE TABLE users (
+    id IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    surname TEXT,
+    age INTEGER,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    phone_number VARCHAR(55),
+    avatar TEXT,
+    account_type VARCHAR(50)
 );
 
-insert into USERS(name, surName, age, email, password,phoneNumber, avatar, accountType)
-values('Giorno', 'Giovanno', 17, 'goldenwind@mudamuda', 'mudamuda', '123','', 'EMPLOYER'),
-      ('Josuke', 'Higashikata', 18, 'crazyDia@dora', 'doradora', '6543', '', 'EMPLOYEE');
-
-
-
-create table if not exists VACANCIES(
-                                        id identity primary key,
-                                        name varchar(45),
-                                        description varchar(255),
-                                        category varchar(45),
-                                        salary double,
-                                        expFrom int,
-                                        expTo int,
-                                        isActive boolean,
-                                        author int,
-                                        createdDate timestamp,
-                                        updateTime timestamp
+CREATE TABLE categories (
+    id IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    parent_id INTEGER,
+        FOREIGN KEY (parent_id) REFERENCES categories(id)
 );
 
-insert into VACANCIES(name, description, category, salary, expFrom,expTo, isActive, author, createdDate)
-values ( 'Plumber', 'need a plumber', 'Labor', 250, 0,3,true,1, LOCALTIMESTAMP()),
-       ( 'Cook', 'meth', 'Mental', 2450, 3,6,true,1, LOCALTIMESTAMP());
-
-create table if not exists RESUMES(
-                                      id identity primary key,
-                                      author int,
-                                      name varchar(45),
-                                      category varchar(45),
-                                      salary double,
-                                      isActive boolean,
-                                      createdDate timestamp,
-                                      updateTime timestamp
+CREATE TABLE contact_types (
+    id IDENTITY PRIMARY KEY,
+    type TEXT NOT NULL
 );
 
-insert into RESUMES(author, name, category, salary,  isActive,  createdDate)
-values ( 2, 'Pro Plumber', 'Labor', 150, true, LOCALTIMESTAMP() ),
-       ( 2, 'Pro Cook', 'Mental', 1502, false, LOCALTIMESTAMP() );
+
+CREATE TABLE resumes (
+    id IDENTITY PRIMARY KEY,
+    applicant_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    category_id INTEGER NOT NULL,
+    salary REAL,
+    is_active BOOLEAN NOT NULL,
+    created_date TIMESTAMP NOT NULL,
+    update_time TIMESTAMP NOT NULL,
+        FOREIGN KEY (applicant_id) REFERENCES users(id),
+        FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE contact_info (
+    id IDENTITY PRIMARY KEY,
+    resume_id INTEGER NOT NULL,
+    type_id INTEGER NOT NULL,
+    "value" TEXT NOT NULL,
+        FOREIGN KEY (resume_id) REFERENCES resumes(id),
+        FOREIGN KEY (type_id) REFERENCES contact_types(id)
+);
+
+CREATE TABLE education_info (
+    id IDENTITY PRIMARY KEY,
+    resume_id INTEGER NOT NULL,
+    institution TEXT NOT NULL,
+    program TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    degree TEXT,
+        FOREIGN KEY (resume_id) REFERENCES resumes(id)
+);
+
+CREATE TABLE work_experience_info (
+    id IDENTITY PRIMARY KEY,
+    resume_id INTEGER NOT NULL,
+    years INTEGER,
+    company_name TEXT NOT NULL,
+    position TEXT NOT NULL,
+    responsibilities TEXT,
+        FOREIGN KEY (resume_id) REFERENCES resumes(id)
+);
+
+CREATE TABLE vacancies (
+    id IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    category_id INTEGER NOT NULL,
+    salary REAL,
+    exp_from INTEGER,
+    exp_to INTEGER,
+    is_active BOOLEAN NOT NULL,
+    author_id INTEGER NOT NULL,
+    created_date TIMESTAMP NOT NULL,
+    update_time TIMESTAMP NOT NULL,
+        FOREIGN KEY (category_id) REFERENCES categories(id),
+        FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE responded_applicants (
+    id IDENTITY PRIMARY KEY,
+    resume_id INTEGER NOT NULL,
+    vacancy_id INTEGER NOT NULL,
+    confirmation BOOLEAN,
+        FOREIGN KEY (resume_id) REFERENCES resumes(id),
+        FOREIGN KEY (vacancy_id) REFERENCES vacancies(id)
+);
+
+CREATE TABLE messages (
+    id IDENTITY PRIMARY KEY,
+    responded_applicants_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    FOREIGN KEY (responded_applicants_id) REFERENCES responded_applicants(id)
+);
+
+
+INSERT INTO users (name, surname, age, email, password, phone_number, avatar, account_type ) VALUES
+    ('Billy', 'Herryngton', 37, 'gachi@muchi.com', 'password123', '1234567890', '', 'applicant'),
+    ('Van', 'Darkholme', 45, 'boss@ofgym.com', 'secure', '0987654321', '', 'employer');
+
+INSERT INTO categories (name) VALUES ('IT'), ('Finances'), ('Education');
+INSERT INTO categories (name, parent_id) VALUES ('Software Development', 1), ('Analytics', 2);
+
+INSERT INTO contact_types (type) VALUES ('Phone'), ('Email'), ('Telegram');
+
+INSERT INTO resumes (applicant_id, name, category_id, salary, is_active, created_date, update_time) VALUES
+    (1, 'Java the Right Version ♂', 4, 15000.00, TRUE, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO contact_info (resume_id, type_id, "value") VALUES
+    (1, 1, '1234567890'),
+    (1, 2, 'gachi@muchi.com');
+
+
+INSERT INTO vacancies (name, description, category_id, salary, exp_from, is_active, author_id, created_date, update_time) VALUES
+    ('Java Developer', 'need someone with straight hands', 4, 180000.00, 3, TRUE, 2, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+INSERT INTO education_info (resume_id, institution, program, start_date, end_date, degree) VALUES
+    (1, 'University', 'Computer Science', '2010-09-01', '2015-06-25', 'Professor');
+
+INSERT INTO work_experience_info (resume_id, years,  company_name, position, responsibilities) VALUES
+    (1, 2, 'Right Version Company','Junior', 'Support & Assist');
+
+INSERT INTO responded_applicants (resume_id, vacancy_id, confirmation) VALUES
+    (1, 1, TRUE);
+
+INSERT INTO messages (responded_applicants_id, content, timestamp) VALUES
+    (1, 'Im interested in vacancy you have posted', CURRENT_TIMESTAMP());
