b63997f616e1710d69c8a2dc2d8b166cbfcc8d36
name <tzolcubekov@gmail.com>
2025-12-19 23:36:41 +0600

    Added dto class for Resume entity

---
+++ src/main/java/springboot/get_a_job/dto/ResumeDto.java
@@ -0,0 +1,20 @@
+package springboot.get_a_job.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResumeDto {
+    Integer applicantId;
+    String name;
+    String category;
+    Double salary;
+    List<EducationDto> education;
+    List<WorkExperienceDto> workExperience;
+}
