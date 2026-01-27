86e223f9a54042d45228bcad45a8d66288c2693a
name <tzolcubekov@gmail.com>
2026-01-27 14:04:25 +0600

    Clenaed Controller classes code

--- src/main/java/springboot/get_a_job/controllers/ResumeController.java
+++ src/main/java/springboot/get_a_job/controllers/ResumeController.java
@@ -29,8 +29,6 @@ import java.util.stream.Collectors;
 public class ResumeController {
     private final CategoryService categoryService;
     private final ResumeService resumeService;
-    private final EducationInfoService educationInfoService;
-    private final WorkExperienceService workExperienceService;
     private final ContactInfoService contactInfoService;
 
     @GetMapping("/form")
@@ -151,106 +149,4 @@ public class ResumeController {
         model.addAttribute("resume", resume);
         return "resume";
     }
-
-
-
-    //    @PutMapping("/update/{id}")
-//    public ResponseEntity<String> updateResume(@Validated(OnUpdate.class)  @PathVariable Integer id, @RequestBody ResumeDto resumeDto) {
-//        resumeService.updateResume(id, resumeDto);
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body("Resume successfully updated");
-//    }
-//
-//    @DeleteMapping("/delete/{id}")
-//    public ResponseEntity<String> deleteResume(@PathVariable Integer id) {
-//        resumeService.deleteResume(id);
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body("Resume successfully deleted");
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<ResumeDto> findResumeById(@PathVariable Integer id) {
-//        return resumeService.findResumeById(id)
-//                .map(ResponseEntity::ok)
-//                .orElseGet(() -> ResponseEntity.notFound().build());
-//    }
-//
-//    @GetMapping("/categoryId/{category_id}")
-//    public ResponseEntity<List<ResumeDto>> findResumeByCategoryId(@PathVariable Integer category_id) {
-//        return resumeService.findResumeByCategory(category_id)
-//                .map(ResponseEntity::ok)
-//                .orElseGet(() -> ResponseEntity.notFound().build());
-//    }
-//
-//    @GetMapping("/category/{category}")
-//    public ResponseEntity<List<ResumeDto>> findResumeByCategory(@PathVariable String category) {
-//        return resumeService.findResumeByCategory(category)
-//                .map(ResponseEntity::ok)
-//                .orElseGet(() -> ResponseEntity.notFound().build());
-//    }
-//
-//    @GetMapping("/creatorId/{applicant_id}")
-//    public ResponseEntity<List<ResumeDto>> findResumeByCreatorId(@PathVariable Integer applicant_id) {
-//        return resumeService.findResumeByCreator(applicant_id)
-//                .map(ResponseEntity::ok)
-//                .orElseGet(() -> ResponseEntity.notFound().build());
-//    }
-//
-//    @GetMapping("/creator/{name}")
-//    public ResponseEntity<List<ResumeDto>> findResumeByCreatorName(@PathVariable String name) {
-//        return resumeService.findResumeByCreator(name)
-//                .map(ResponseEntity::ok)
-//                .orElseGet(() -> ResponseEntity.notFound().build());
-//    }
-//
-//    //figuring out
-//    @PostMapping("add/education/{resumeId}")
-//    public ResponseEntity<String> addEducation(@Validated(OnCreate.class) @PathVariable Integer resumeId, @RequestBody List<EducationDto> educationDto) {
-//        educationInfoService.addEducationInfo(resumeId, educationDto);
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body("Education info for Resume " + resumeId + " successfully added");
-//    }
-//
-//    @PutMapping("update/education/{educationId}")
-//    public ResponseEntity<String> updateEducation(@Validated(OnUpdate.class) @PathVariable Integer educationId, @RequestBody List<EducationDto> educationDto) {
-//        educationInfoService.updateResumesEducationInfo(educationDto);
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body("Education info successfully updated");
-//    }
-//
-//    @PostMapping("add/work_exp/{resumeId}")
-//    public ResponseEntity<String> addWorkExperience(@Validated(OnCreate.class) @PathVariable Integer resumeId, @RequestBody List<WorkExperienceDto> workExperienceDtos) {
-//        workExperienceService.addWorkExperienceInfo(resumeId, workExperienceDtos);
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body("Work Experience info for Resume " + resumeId + " successfully added");
-//    }
-//
-//    @PutMapping("update/work_exp/{workExpId}")
-//    public ResponseEntity<String> updateWorkExperience(@Validated(OnUpdate.class) @PathVariable Integer workExpId, @RequestBody List<WorkExperienceDto> workExperienceDto) {
-//        workExperienceService.updateResumesWorkExperienceInfo(workExperienceDto);
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body("Work Experience info successfully updated");
-//    }
-//
-//    @PostMapping("add/contact_info/{resumeId}")
-//    public ResponseEntity<String> addContactInfo(@Validated(OnCreate.class) @PathVariable Integer resumeId, @RequestBody List<ContactInfoDto> contactInfoDtos) {
-//        contactInfoService.addContactInfo(resumeId, contactInfoDtos);
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body("Contact info for Resume " + resumeId + " successfully added");
-//    }
-//
-//    @PutMapping("update/contact_info/{contactInfoId}")
-//    public ResponseEntity<String> updateContactInfo(@Validated(OnUpdate.class) @PathVariable Integer contactInfoId, @RequestBody List<ContactInfoDto> contactInfoDtos) {
-//        contactInfoService.updateContactInfo(contactInfoDtos);
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body("Contact info successfully updated");
-//    }
 }
